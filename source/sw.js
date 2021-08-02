d7425a20838c7837f34b0461e5b3d9dbc93b7b60
Sebastian Friedrich <dev@sebastianfriedrich.at>
2021-08-02 17:34:05 +0200

    user-select: none; autoprefixer

--- source/sw.js
+++ source/sw.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1-<PROD_HASH_REPLACED_DURING_BUILD>';
-const RUNTIME = 'runtime-v1-<PROD_HASH_REPLACED_DURING_BUILD>';
+const PRECACHE = 'precache-<PROD_HASH_REPLACED_DURING_BUILD>';
+const RUNTIME = 'runtime-<PROD_HASH_REPLACED_DURING_BUILD>';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
--- www/sw.js
+++ www/sw.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1-a63b0fc0941420512189e857243b45d4';
-const RUNTIME = 'runtime-v1-a63b0fc0941420512189e857243b45d4';
+const PRECACHE = 'precache-v1-c2ee57fc73f8f22d87dd0051b5dccb9d';
+const RUNTIME = 'runtime-v1-c2ee57fc73f8f22d87dd0051b5dccb9d';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
